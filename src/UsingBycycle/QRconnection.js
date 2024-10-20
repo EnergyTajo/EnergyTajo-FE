9c7b5f3de181de0d3e62659f7993be762511f901
jwonny0118 <jwonny0118@gmail.com>
2024-10-20 23:24:47 +0900

    🔨 Fix : 빌드 오류 수정 및 재푸쉬3

--- src/UsingBycycle/QRconnection.js
+++ src/UsingBycycle/QRconnection.js
@@ -13,7 +13,10 @@ function QRconnection() {
     }
   };
 
-  const handleError = (err) => {};
+  const handleError = () => {
+    // 에러 핸들링을 위한 코드가 필요하다면 이곳에 추가하세요.
+    // 현재는 에러를 무시하고 있습니다.
+  };
 
   return (
     <div>
@@ -21,8 +24,8 @@ function QRconnection() {
       {showScanner ? (
         <QrScanner
           delay={300}
-          onError={handleError}
-          onScan={handleScan}
+          onError={handleError} // 에러가 발생하면 handleError가 호출됩니다.
+          onScan={handleScan} // QR 코드 스캔 성공 시 handleScan이 호출됩니다.
           style={{ width: '100%' }}
         />
       ) : (
